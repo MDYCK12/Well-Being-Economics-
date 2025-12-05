ec140558b1c24a80600df7e2d9b523aa1e4f620b
artkl1 <artkli@yahoo.com>
2025-12-05 13:39:59 +0100

    Update mypage.py

--- Page/mypage.py
+++ Page/mypage.py
@@ -46,6 +46,8 @@ st.markdown(
 # -----------------------------------
 df_url = "https://docs.google.com/spreadsheets/d/1E0lyCSxlC0ajNtzjpWo17TX5DEeEjd33E-j6c7fOBcg/export?format=csv"
 
+# @st.cache_data
+
 
 def load_data():
     return pd.read_csv(df_url)
