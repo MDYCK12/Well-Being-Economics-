5d65f2f369a17a5b31626a5d193194f46eba29de
artkl1 <artkli@yahoo.com>
2025-12-10 17:25:33 +0100

    Update functions.py

--- Functions/functions.py
+++ Functions/functions.py
@@ -714,9 +714,9 @@ def plot_esi_wti_quadrants(df_merged_scores: pd.DataFrame):
 
     fig.update_traces(
         mode='markers+text',
-        marker=dict(size=40),
+        marker=dict(size=30),
         textposition='top right',
-        textfont=dict(size=10, color='white')
+        textfont=dict(size=17, color='white')
     )
 
     fig.add_hline(y=0, line_width=1, line_dash="dash", line_color="red", annotation_text="WTI Average (0)")
